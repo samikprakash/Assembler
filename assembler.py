b89ad01737249eb540f87179566492383b746dc4
Samik Prakash <42117804+samikprakash@users.noreply.github.com>
2019-10-08 22:57:32 +0530

    change1

--- assembler.py
+++ assembler.py
@@ -1,6 +1,6 @@
 # mapping instructions to the corresponding opcode :-
 
-instruction_to_opcode_dict = {"CLA":"0000","LAC":"0001","SAC":"0010","ADD":"0011","SUB":"0100",
+opcode_table = {"CLA":"0000","LAC":"0001","SAC":"0010","ADD":"0011","SUB":"0100",
 "BRZ":"0101","BRN":"0110","BRP":"0111","INP":"1000","DSP":"1001","MUL":"1010","DIV":"1011","STP":"1100"} 
 
 file = open("sample_input.txt","r")
@@ -12,23 +12,24 @@ def symbol_exists(instruction):   #checks if instruction line contains symbol or
         return False
     return True
 
-def make_symbol_table(symbol_table,instruction):  #takes symbol out of instruction line and adds it to symbol table
+def make_symbol_table(symbol_table,instruction,ilc):  #takes symbol out of instruction line and adds it to symbol table
     temp = instruction.replace(" ","")
     if(symbol_exists(instruction)):
-        symbol_table.append(instruction[:instruction.find(":")])
+        symbol_table[instruction[:instruction.find(":")]] = ilc
     return
     
     
-symbol_table = []
+symbol_table = {}
 
-
+instruction_location_counter = 0
 
 instructions = []
 for i in file.readlines():
     # print(i)
     if i!='\n':
         instructions.append(i)
-        make_symbol_table(symbol_table,i)
+        make_symbol_table(symbol_table,i,instruction_location_counter)
+        instruction_location_counter+=12 
     
 file.close()
 
