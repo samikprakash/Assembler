d4220c2ab9ce763d7f2c36e0f749a91ad38dc0d3
Samik Prakash <42117804+samikprakash@users.noreply.github.com>
2019-10-08 15:36:49 +0530

    Add files via upload

---
+++ assembler.py
@@ -0,0 +1,35 @@
+# mapping instructions to the corresponding opcode :-
+
+instruction_to_opcode_dict = {"CLA":"0000","LAC":"0001","SAC":"0010","ADD":"0011","SUB":"0100",
+"BRZ":"0101","BRN":"0110","BRP":"0111","INP":"1000","DSP":"1001","MUL":"1010","DIV":"1011","STP":"1100"} 
+
+file = open("sample_input.txt","r")
+
+
+def symbol_exists(instruction):   #checks if instruction line contains symbol or not
+    temp = instruction.replace(" ","")
+    if temp.find(":") == -1:
+        return False
+    return True
+
+def make_symbol_table(symbol_table,instruction):  #takes symbol out of instruction line and adds it to symbol table
+    temp = instruction.replace(" ","")
+    if(symbol_exists(instruction)):
+        symbol_table.append(instruction[:instruction.find(":")])
+    return
+    
+    
+symbol_table = []
+
+
+
+instructions = []
+for i in file.readlines():
+    # print(i)
+    if i!='\n':
+        instructions.append(i)
+        make_symbol_table(symbol_table,i)
+    
+file.close()
+print(instructions[0])
+print(symbol_table)
